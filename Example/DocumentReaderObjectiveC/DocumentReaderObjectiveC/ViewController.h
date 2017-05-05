4575023f654498a0b59722b7431750cb017c5ff9
Dmitry Smolyakov <dm.smolyakov@gmail.com>
2017-05-05 16:41:14 +0300

    Added empty Objective C example

---
+++ Example/DocumentReaderObjectiveC/DocumentReaderObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DocumentReaderObjectiveC
+//
+//  Created by Dmitry Smolyakov on 5/5/17.
+//  Copyright © 2017 Dmitry Smolyakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
